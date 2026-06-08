74d0087784db921acc43925f31200865d85cfbcc
MahakAg64 <mahakagarwal0618@gmail.com>
2026-06-08 23:12:50 +0530

    Add files via upload

---
+++ WEEK 2/OOPS-WEEK-2/Veena.java
@@ -0,0 +1,9 @@
+package music.string;
+import music.Playble;
+
+public class Veena implements Playble{
+
+    public void play(){
+        System.out.println("This method name is Veena and it is  implementing Playble interface!");
+    }
+}
